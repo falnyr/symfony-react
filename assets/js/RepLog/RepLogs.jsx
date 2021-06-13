1a45ae5de82d30427e992bf46176e20f5dc496b1
Jan Richter <falnyr@gmail.com>
2021-06-13 12:42:48 +1200

    feat: preparing for Refs

--- assets/js/RepLog/RepLogs.jsx
+++ assets/js/RepLog/RepLogs.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import RepLogList from "./RepLogList";
 import PropTypes from "prop-types";
+import RepLogCreator from "./RepLogCreator";
 
 function calculateTotalWeightLifted(repLogs) {
   let total = 0
@@ -17,14 +18,6 @@ export default function RepLogs(props) {
   let heart = ''
   if (withHeart) {
     heart = <span>❤️</span>
-  }
-
-  function handleFormSubmit(event) {
-    event.preventDefault()
-
-    alert(event.target.elements.namedItem('reps').value)
-
-    onNewItemSubmit("Big Fat Cat", event.target.elements.namedItem('reps').value)
   }
 
   return (
@@ -55,37 +48,7 @@ export default function RepLogs(props) {
         </tfoot>
       </table>
 
-
-      <form className="form-inline" onSubmit={handleFormSubmit}>
-        <div className="form-group">
-          <label className="sr-only control-label required" htmlFor="rep_log_item">
-            What did you lift?
-          </label>
-          <select id="rep_log_item"
-                  name="item"
-                  required="required"
-                  className="form-control">
-            <option value="">What did you lift?</option>
-            <option value="cat">Cat</option>
-            <option value="fat_cat">Big Fat Cat</option>
-            <option value="laptop">My Laptop</option>
-            <option value="coffee_cup">Coffee Cup</option>
-          </select>
-        </div>
-        {' '}
-        <div className="form-group">
-          <label className="sr-only control-label required" htmlFor="rep_log_reps">
-            How many times?
-          </label>
-          <input type="number" id="rep_log_reps"
-                 name="reps" required="required"
-                 placeholder="How many times?"
-                 className="form-control"/>
-        </div>
-        {' '}
-        <button type="submit" className="btn btn-primary">I Lifted it!</button>
-      </form>
-
+      <RepLogCreator onNewItemSubmit={onNewItemSubmit} />
 
     </div>
   );
